d1e0f3bff69b6efd06062f92fb7519475a1ccc26
szzd7223 <mohdsaadshaikh17@gmail.com>
2026-01-09 03:50:11 +0530

    colours tab fix

--- extension-ui/src/components/tabs/ColorsTab.jsx
+++ extension-ui/src/components/tabs/ColorsTab.jsx
@@ -10,7 +10,6 @@ export default function ColorsTab({ selectedElement, onUpdateElement }) {
 
     useEffect(() => {
         if (selectedElement) {
-            // I will fix this
             const initialState = {
                 color: selectedElement.colors.text,
                 backgroundColor: selectedElement.colors.background,
